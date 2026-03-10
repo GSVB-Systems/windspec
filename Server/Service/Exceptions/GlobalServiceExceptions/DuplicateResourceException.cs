3cbb8f31e90a54bfa4691d54820a9a01793a109c
MathiasFIv <mathiasfriisiversen@gmail.com>
2026-03-10 14:36:35 +0100

    CommandLog velidation/exceptions
    validation for CommandLog

---
+++ Server/Service/Exceptions/GlobalServiceExceptions/DuplicateResourceException.cs
@@ -0,0 +1,9 @@
+﻿namespace service.Exceptions
+{
+    public class DuplicateResourceException : RuleValidationException
+    {
+        public DuplicateResourceException() { }
+        public DuplicateResourceException(string message) : base(message) { }
+        public DuplicateResourceException(string message, System.Exception inner) : base(message, inner) { }
+    }
+}
---
+++ Server/Service/Exceptions/GlobalServiceExceptions/RuleValidationException.cs
@@ -0,0 +1,9 @@
+﻿namespace service.Exceptions
+{
+    public class RuleValidationException : System.Exception
+    {
+        public RuleValidationException() { }
+        public RuleValidationException(string message) : base(message) { }
+        public RuleValidationException(string message, System.Exception inner) : base(message, inner) { }
+    }
+}
